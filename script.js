082169fbc4331bfb9240f35a074184a33212d700
Gustavo zero bala <98385777+gustawebp@users.noreply.github.com>
2022-05-24 01:25:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+
+function c1() {
+    let res = document.getElementById('res')
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2 = parseInt(document.getElementById('n2').value)
+
+
+    let res1 = (n1+n2)
+   res.innerHTML = `${res1}`
+
+
+}
+
+
+function c2() {
+    let res = document.getElementById('res')
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2 = parseInt(document.getElementById('n2').value)
+
+
+    let res1 = (n1*n2)
+   res.innerHTML = `${res1}`
+
+
+}
+
+function c3() {
+    let res = document.getElementById('res')
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2 = parseInt(document.getElementById('n2').value)
+
+
+    let res1 = (n1-n2)
+   res.innerHTML = `${res1}`
+
+
+}
+
+
+function c4() {
+    let res = document.getElementById('res')
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2 = parseInt(document.getElementById('n2').value)
+
+
+    let res1 = (n1/n2)
+   res.innerHTML = `${res1}`
+
+
+}
+
+
+function c5() {
+    let res = document.getElementById('res')
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2 = parseInt(document.getElementById('n2').value)
+
+  
+    let res1 = (n1*n1)
+   res.innerHTML = `${res1}`
+   
+
+
+}
+
+
+function c6() {
+    let res = document.getElementById('res')
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2 = parseInt(document.getElementById('n2').value)
+    let pi = 3.14159265
+  
+    let res1 = (n1 * pi )
+   res.innerHTML = `${res1}`
+   
+
+
+}
+
+
+
+
+
+//3,14159265
